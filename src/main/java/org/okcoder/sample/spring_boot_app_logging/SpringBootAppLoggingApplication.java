99ae1c8b4c662df7d5ce051ab43035ff7a40db74
okcoder <okcoder@live.com>
2023-06-05 20:18:59 +0900

    create project from start.spring.io

---
+++ src/main/java/org/okcoder/sample/spring_boot_app_logging/SpringBootAppLoggingApplication.java
@@ -0,0 +1,13 @@
+package org.okcoder.sample.spring_boot_app_logging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAppLoggingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAppLoggingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/okcoder/sample/spring_boot_app_logging/SpringBootAppLoggingApplicationTests.java
@@ -0,0 +1,13 @@
+package org.okcoder.sample.spring_boot_app_logging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAppLoggingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
